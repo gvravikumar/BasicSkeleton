e75ea20c4edfa3272f5d3978fc9a6b977899e452
G V RAVI KUMAR <gvpraveen.ravi@gmail.com>
2019-09-01 11:52:04 +0530

    MVVM ButterKnife

---
+++ app/src/main/java/com/gvrk/getmylostmobile/BasicActivity.java
@@ -0,0 +1,6 @@
+package com.gvrk.getmylostmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/gvrk/getmylostmobile/MainApp.java
@@ -0,0 +1,7 @@
+package com.gvrk.getmylostmobile;
+
+import android.app.Application;
+
+public class MainApp extends Application {
+
+}
